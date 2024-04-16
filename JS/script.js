64e49abf3ad6eff7a9d9f24b65ebc742c383d549
Hector2414 <hectorlaucher6@gmail.com>
2024-04-16 09:29:51 -0300

    mexendo com API do rick and morty

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+const imagem = document.querySelector(".imagem");
+const container = document.querySelector(".container");
+const situacao = document.querySelector(".status");
+const especie = document.querySelector(".especie");
+const origem = document.querySelector(".origem");
+
+const url = 'https://rickandmortyapi.com/api/character';
+
+function RickAndMorty(){
+    fetch(url,{
+        method:'GET',
+    })
+    .then((reposta) => reposta.json())
+    .then((dados)=>{
+       imprimirDados(dados)
+       console.log(dados)
+    })
+    .catch(erro => alert(erro))
+}
+
+function imprimirDados(dados){
+    dados.results.forEach(element => {
+        let p = document.createElement('p');
+        p.setAttribute('id', element.id);
+        p.innerHTML = element.name;
+        container.appendChild(p)
+        
+
+        p.addEventListener('click', () =>{
+             imagem.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/${element.id}.jpeg">`
+             situacao.innerHTML = `A situação do personagem é: ${element.status}`,
+             origem.innerHTML = `A origem do personagem é :${element.origin.name}`,
+             especie.innerHTML = `a especie do personagem é : ${element.species}`
+        } )
+
+           
+    });
+    
+}
+
+RickAndMorty()
+
